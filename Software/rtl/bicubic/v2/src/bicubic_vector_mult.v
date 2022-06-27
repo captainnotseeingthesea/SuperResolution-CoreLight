bb15bbf96966594a7ab6c3902ee8be85d2340936
gao12 <2117848210@qq.com>
2022-06-27 12:04:32 +0800

    modified the width of vector_mult

--- Software/rtl/bicubic/v2/src/bicubic_vector_mult.v
+++ Software/rtl/bicubic/v2/src/bicubic_vector_mult.v
@@ -18,41 +18,41 @@ module bicubic_vector_mult #
 
 
     wire [PRODUCT_WIDTH - 1:0] product_t1, product_t2, product_t3, product_t4;
-    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH - 1)) u_bicubic_mult_1(
+    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH)) u_bicubic_mult_1(
         .weight       (weight_1),
         .pixel        (pixel_1[PRODUCT_WIDTH -8 - 1:0]),
         .product      (product_t1)
     );
-    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH - 1)) u_bicubic_mult_2(
+    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH)) u_bicubic_mult_2(
         .weight       (weight_2),
         .pixel        (pixel_2[PRODUCT_WIDTH -8 - 1:0]),
         .product      (product_t2)
     );
-    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH - 1)) u_bicubic_mult_3(
+    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH)) u_bicubic_mult_3(
         .weight       (weight_3 ),
         .pixel        (pixel_3[PRODUCT_WIDTH -8 - 1:0]),
         .product      (product_t3)
     );
-    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH - 1)) u_bicubic_mult_4(
+    bicubic_mult #(.PRODUCT_WIDTH(PRODUCT_WIDTH)) u_bicubic_mult_4(
         .weight       (weight_4),
         .pixel        (pixel_4[PRODUCT_WIDTH -8 - 1:0]),
         .product      (product_t4)
     );
 
-    wire [PRODUCT_WIDTH:0] adder1_src1 = product_t1;
-    wire [PRODUCT_WIDTH:0] adder1_src2 = product_t2;
+    wire [PRODUCT_WIDTH-1:0] adder1_src1 = product_t1;
+    wire [PRODUCT_WIDTH-1:0] adder1_src2 = product_t2;
 
-    wire [PRODUCT_WIDTH:0] adder2_src1 = product_t3;
-    wire [PRODUCT_WIDTH:0] adder2_src2 = product_t4;   
+    wire [PRODUCT_WIDTH-1:0] adder2_src1 = product_t3;
+    wire [PRODUCT_WIDTH-1:0] adder2_src2 = product_t4;   
 
-    wire [PRODUCT_WIDTH:0] result1 = adder1_src1 + adder1_src2;
-    wire [PRODUCT_WIDTH:0] result2 = adder2_src1 + adder2_src2;
+    wire [PRODUCT_WIDTH-1:0] result1 = adder1_src1 + adder1_src2;
+    wire [PRODUCT_WIDTH-1:0] result2 = adder2_src1 + adder2_src2;
 
     // wire result1_overflow = result1[PRODUCT_WIDTH] ^ result1[PRODUCT_WIDTH - 1];
     // wire result2_overflow = result2[PRODUCT_WIDTH] ^ result2[PRODUCT_WIDTH - 1];
 
 
-    wire [PRODUCT_WIDTH:0] result3 = result1 + result2;
+    wire [PRODUCT_WIDTH-1:0] result3 = result1 + result2;
 
     // wire result3_overflow = result3[PRODUCT_WIDTH] ^ result3[PRODUCT_WIDTH - 1];
 
